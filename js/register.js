7399f6864389ebc76f63b38081e099e7425acd1b
Gry0u <gauthriou@protonmail.com>
2018-10-11 00:08:29 +0200

    Typo

--- js/register.js
+++ js/register.js
@@ -3,5 +3,5 @@ if (navigator.serviceWorker) {
   navigator.serviceWorker
     .register('/sw.js')
     .then(reg => console.log("Registration ok"))
-    .catch(err => console.log('Registration failed' + err))
+    .catch(err => console.log('Registration failed' + err));
 }
